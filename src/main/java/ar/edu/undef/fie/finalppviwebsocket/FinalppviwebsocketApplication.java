eb7584bef5e4536862120089250eeb268a39e7eb
Jonathan Curellich <viking@192.168.1.50>
2024-02-20 21:44:06 -0300

    First commit

---
+++ src/main/java/ar/edu/undef/fie/finalppviwebsocket/FinalppviwebsocketApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.undef.fie.finalppviwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalppviwebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalppviwebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/undef/fie/finalppviwebsocket/FinalppviwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undef.fie.finalppviwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalppviwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
